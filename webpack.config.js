69f7cc604f06b35a166016f52feaaa8febaaa4c7
duthienkt <blueskythien2010@live.com>
2020-06-26 23:01:50 +0700

    + webpack config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+
+var packages = {
+    default: {
+        entry: ["./dev.js"],
+        filename: "./dist/absol-js-code-preivew.js"
+    },
+    slave: {
+        entry: ['./slave.js'],
+        filename: "./dist/absol-js-code-preivew-slave.js"
+    }
+}
+
+const PACKAGE = 'slave';
+
+
+module.exports = {
+    mode: process.env.MODE || "development",
+    // mode: 'production',
+    entry: packages[PACKAGE].entry,
+    output: {
+        path: path.join(__dirname, "."),
+        filename: packages[PACKAGE].filename
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, './node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: [['@babel/preset-env', { modules: false }]] }
+            },
+            {
+                test: /\.(tpl|txt|xml|rels)$/i,
+                use: 'raw-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    optimization: {
+        minimize: false
+    },
+    devServer: {
+        compress: true,
+        disableHostCheck: true,
+        host: '0.0.0.0'
+    },
+    performance: {
+        hints: false
+    }
+};
